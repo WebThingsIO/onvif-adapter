54433da9e625646f04c2610dda9ac0dff6cd149a
Michael Stegeman <michael@stegeman.me>
2018-12-13 14:22:55 -0700

    Update package script.

--- package.sh
+++ package.sh
@@ -3,7 +3,7 @@
 rm -rf node_modules
 npm install --production
 rm -f SHA256SUMS
-sha256sum package.json *.js LICENSE > SHA256SUMS
+sha256sum package.json *.js lib/*.js LICENSE > SHA256SUMS
 find node_modules -type f -exec sha256sum {} \; >> SHA256SUMS
 TARFILE=$(npm pack)
 tar xzf ${TARFILE}
