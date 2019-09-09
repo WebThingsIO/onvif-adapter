642ab16a7d1449920ee2ad20e93ea121c9ad1cdf
Michael Stegeman <michael@stegeman.me>
2019-09-09 09:00:03 -0800

    Check for ffmpeg before starting up. (#18)
    Fixes #17

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 'use strict';
 
+const child_process = require('child_process');
 const OnvifAdapter = require('./lib/onvif-adapter');
 
-module.exports = (addonManager, manifest) => {
+module.exports = (addonManager, manifest, errorCallback) => {
+  const proc = child_process.spawnSync(
+    'ffmpeg',
+    ['-version'],
+    {encoding: 'utf8'}
+  );
+  if (proc.status !== 0) {
+    errorCallback(manifest.name, 'ffmpeg is not installed');
+    return;
+  }
+
   new OnvifAdapter(addonManager, manifest);
 };
