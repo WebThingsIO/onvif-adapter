fa1c6fb9a694581b0c9bdbf77f11c35457cb01b3
Michael Stegeman <michael@stegeman.me>
2019-09-18 14:30:27 -0800

    Add actions to start/stop transcode. (#19)
    Also, add a manifest.json.

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 'use strict';
 
 const child_process = require('child_process');
+const manifest = require('./manifest.json');
 const OnvifAdapter = require('./lib/onvif-adapter');
 
-module.exports = (addonManager, manifest, errorCallback) => {
+module.exports = (addonManager, _, errorCallback) => {
   const proc = child_process.spawnSync(
     'ffmpeg',
     ['-version'],
     {encoding: 'utf8'}
   );
   if (proc.status !== 0) {
-    errorCallback(manifest.name, 'ffmpeg is not installed');
+    errorCallback(manifest.id, 'ffmpeg is not installed');
     return;
   }
 
-  new OnvifAdapter(addonManager, manifest);
+  new OnvifAdapter(addonManager);
 };
