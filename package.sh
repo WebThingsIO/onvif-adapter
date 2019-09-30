94748e2d9775a75c2f91988eb19bc7c2f4791bcd
Michael Stegeman <michael@stegeman.me>
2019-09-30 11:38:22 -0800

    Switch stream active to a property.

--- package.sh
+++ package.sh
@@ -4,6 +4,7 @@ rm -rf node_modules
 npm install --production
 rm -f SHA256SUMS
 sha256sum manifest.json package.json *.js lib/*.js LICENSE README.md > SHA256SUMS
+rm -rf node_modules/.bin
 find node_modules -type f -exec sha256sum {} \; >> SHA256SUMS
 TARFILE=$(npm pack)
 tar xzf ${TARFILE}
